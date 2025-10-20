ab3ad681345d8071fb9dc864427b0000e4f5bf93
Gentlecoder1 <israeloloruntoba3@gmail.com>
2025-10-20 04:34:44 +0100

    Initial commit: Setup project with Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Aos from 'aos'
+
+function App() {
+  
+  return (
+    <>
+      <div className="bg-green-500 border-2 border-red rounded-2xl p-10 w-fit h-fit text-2xl">PRAISE THE LORD</div>
+      <div className="bg-green-500 border-2 border-red rounded-2xl p-10 w-fit h-fit text-2xl">HALLELUYA</div>
+    </>
+  )
+}
+
+export default App
